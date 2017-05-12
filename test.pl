bc6a13bad31f9543644a03144a3fe698980aa6a4
Reggie <cushingreggie@gmail.com>
2017-05-12 12:59:19 +0200

    clean

--- test.pl
+++ test.pl
@@ -1,5 +1,6 @@
 $ip_ = `docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qahba`;
 $ip = chomp($ip_);
+print($ip);
 $log = `curl -i $ip:8080`;
 $out = `curl -i $ip:8080 2>/dev/null | grep -e ^HTTP | awk '{print \$2}'`;
 if ($out == 200) {
