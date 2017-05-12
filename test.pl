c8120e52fe19701fd3b237cdc615788d99bcc916
Reggie <cushingreggie@gmail.com>
2017-05-12 12:54:28 +0200

    clean

--- test.pl
+++ test.pl
@@ -1,6 +1,6 @@
-
-$log = `curl -i localhost:8080`;
-$out = `curl -i localhost:8080 2>/dev/null | grep -e ^HTTP | awk '{print \$2}'`;
+$ip = `docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' qahba`;
+$log = `curl -i $ip:8080`;
+$out = `curl -i $ip:8080 2>/dev/null | grep -e ^HTTP | awk '{print \$2}'`;
 if ($out == 200) {
 	print($log);
 	exit(0);
