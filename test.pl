c6d2902bea8820c3347a8949ba38701338249ff3
Reggie <cushingreggie@gmail.com>
2017-05-12 12:47:17 +0200

    clean

--- test.pl
+++ test.pl
@@ -1,8 +1,10 @@
+
+$log = `curl -i localhost:8080`;
 $out = `curl -i localhost:8080 2>/dev/null | grep -e ^HTTP | awk '{print \$2}'`;
 if ($out == 200) {
-	print($out);
+	print($log);
 	exit(0);
 } else {
-	print($out);
+	print($log);
 	exit(1);
 }
